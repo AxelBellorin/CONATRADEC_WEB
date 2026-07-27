fed868e84d914b3316d99a9665eeadf741f39419
Axel Bellorin <bellorinaxel3@gmail.com>
2026-07-26 21:34:47 -0600

    aances

--- obj/Release/net9.0/CONATRADEC.AdminWeb.AssemblyInfo.cs
+++ obj/Release/net9.0/CONATRADEC.AdminWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CONATRADEC.AdminWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb23ab30110b376fc2048fbe672c249a770ecc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922cd2f37af5e5e30fc87803e944f0524dceacf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CONATRADEC.AdminWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CONATRADEC.AdminWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
